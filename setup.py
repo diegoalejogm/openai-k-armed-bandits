8189336973be8a94337fb16c9b1cd92a9a4a2551
Diego Gomez Mosquera <diegoalejogm@gmail.com>
2017-12-07 19:35:20 -0500

    Added Pypi metadata.

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup
 
-setup(name='gym_armed_bandits',
-      version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
+setup(
+    name='gym_armed_bandits',
+    version='0.0.1',
+    install_requires=['gym', 'numpy'],
+    packages=['gym_armed_bandits'],
+    description='Environvment built in OpenAI Gym for K Armed Bandits Tasks',
+    author='Diego Gomez',
+    author_email='diegoalejogm@gmail.com',
+    url='https://github.com/diegoalejogm/openai-k-armed-bandits',
+    download_url='https://github.com/diegoalejogm/openai-k-armed-bandits/archive/0.0.1.tar.gz', 
+    keywords=['machine', 'reinforcement', 'learning', 'bandits', 'openai', 'gym'],
+    classifiers=[],
 )
